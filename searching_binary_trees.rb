aabede4473c4ae9e469e391eb4373a846486f901
Oliver <oc@hejverden.dk>
2017-11-13 14:09:44 +0100

    New Node class and Binary Tree class with #build_tree function

--- searching_binary_trees.rb
+++ searching_binary_trees.rb
@@ -1,9 +1,30 @@
 class Node
-  attr_accessor :value, :parent, :children
+  include Enumerable
 
-  def initialize(value = nil, parent = nil, children = [])
-    @value = value
-    @parent = parent
-    @children = children
+  attr_accessor :data, :parent, :left, :right
+
+  def initialize(data = nil, parent = nil)
+    @data = data
   end
 end
+
+class Binary_Tree
+  def build_tree(ary)
+    ary.shuffle!
+    @root = Node.new(ary.shift)
+
+    ary.each do |e|
+      insert_node(e)
+    end
+  end
+
+end
+
+
+
+# [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
+# Get the Middle of the array and make it root.
+# Recursively do same for left half and right half.
+# Get the middle of left half and make it left child of the root created in step 1.
+# Get the middle of right half and make it right child of the root created in step 1.
